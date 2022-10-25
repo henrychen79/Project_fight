5e55e527f3ba8cfcf0436f10bbdc036ba66e102d
henrychen79 <s1990050479@gmail.com>
2022-10-25 14:11:35 +0800

    add data model.

---
+++ server/models/game_data_store.js
@@ -0,0 +1,31 @@
+let instance;
+
+class game_data {
+  constructor() {
+    this.game = [];
+    this.player = {};
+  }
+  check_room_exist(room_id) {
+    console.log(this.game[room_id]);
+    return this.game[room_id];
+  }
+  init_game_data(room_id, game_obj, player_obj) {
+    this.game[room_id] = game_obj;
+    this.player[player_obj.name] = player_obj;
+  }
+  get_player(playerName) {
+    return this.player[playerName];
+  }
+  callMe() {
+    console.log(`Hi! I'm ${this.game}`);
+  }
+}
+
+module.exports = (() => {
+  if (instance) {
+    return instance;
+  }
+
+  instance = new game_data();
+  return instance;
+})();
