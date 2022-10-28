7de99d2793c54487f8c77ab8afbb372396df3dad
Absinthe <h90243768@gmail.com>
2022-10-28 16:51:31 +0800

    modified version

--- server/game_component/player.js
+++ server/game_component/player.js
@@ -1,155 +1,81 @@
-class Player {
-  constructor(
-    player_name,
-    room_id,
-    location,
-    player_role,
-    direction_x,
-    attackLocation_R,
-    attackLocation_L
-  ) {
-    this.name = player_name;
-    this.room_id = room_id;
-    (this.location = location || { x: 0, y: 5, scale: 100 }), //預設為:{x:0,y:0,scale:100}
-      (this.attackLocation_R = { x: 1, y: 5 }),
-      (this.attackLocation_L = { x: -1, y: 5 }),
-      (this.attackPower = false);
-    this.attack = false;
-    this.scaleControl = false;
-    this.jumpControl = true;
-    this.hp=100;
-    this.directionX = direction_x || "ArrowRight";
-    this.directionY = null;
-    this.playerRole = player_role || "player1";
-    this.isWinner=false;
-  }
-  player_moveUp() {
-    if (this.scaleControl == true) {
-      return;
+class Player{
+constructor({position,velocity},name,room){
+    this.name=name,
+    this.room_id=room,
+    this.world_Width=900;
+    this.world_Height=500;
+    this.width=50;
+    this.height=100;
+    this.position=position
+    this.velocity=velocity
+    this.direction=null
+    this.nojump = true
+}
+move(type){
+    this.direction=type
+    //往右移動
+    if(this.direction==='ArrowLeft'){
+        if(this.position.x===0){
+            this.position.x=0
+        }else{
+            this.position.x = this.position.x + this.velocity.x
+        }
+        
+    
     }
-    if (this.jumpControl) {
-      this.location.x = this.location.x;
-      this.location.y = this.location.y - 1;
-      this.jumpControl = false;
+    //往左移動
+    if(this.direction==='ArrowRight'){   
+        if(this.position.x===(this.world_Width-this.width)){
+            this.position.x=850
+        }else{
+            this.position.x = this.position.x + this.velocity.x
+        }
     }
-  }
-  player_moveDown() {
-    if (this.jumpControl != true) {
-      return;
+    //往上移動(keyup觸發，若在上升須修正到頂點在回原點，若在下降，則須修正到原點，修正此問題的function為revise_move)
+    if(this.direction==='ArrowUp'){
+        if(this.nojump===true){
+            if( this.position.y===0){
+                this.nojump=false
+                return
+            }
+            this.velocity.y=-5
+            this.position.y = this.position.y + this.velocity.y
+        }
+        if(this.nojump===false){
+            if( this.position.y>=200){
+                this.nojump=true
+                return
+            }
+            this.velocity.y=5
+            this.position.y = this.position.y + this.velocity.y
+        }
     }
-    this.scaleControl = true;
-    if (this.scaleControl) {
-      this.location.scale = 50;
-    }
-  }
+    // console.log(this.nojump,this.position.y,player1.velocity.y)
+}
 
-  player_moveLeft() {
-    if (this.jumpControl != true || this.scaleControl == true) {
-      return;
-    }
-    if (this.location.x === 0) {
-      this.location.x = this.location.x;
-      this.location.y = this.location.y;
-    } else {
-      this.location.x = this.location.x - 1;
-      this.location.y = this.location.y;
-    }
-  }
+revise_move(type){
+    this.direction=type
+    if(this.direction===''){
+        //下墜修正回原地
+        if(this.velocity.y===-5){
+            if(this.position.y===0){
+                this.velocity.y=0
+                this.nojump = true
+            }
+            this.position.y = this.position.y + this.velocity.y
+        }
 
-  player_moveRight() {
-    if (this.jumpControl != true || this.scaleControl == true) {
-      return;
+        //上升修正到頂點 再下墜回原地
+        if(this.velocity.y===5){
+            this.position.y = this.position.y + this.velocity.y
+            if(this.position.y>=200){
+                this.velocity.y=-5
+                this.nojump = true
+            }  
+        }
     }
-    if (this.location.x === 9) {
-      this.location.x = this.location.x;
-      this.location.y = this.location.y;
-    } else {
-      this.location.x = this.location.x + 1;
-      this.location.y = this.location.y;
-    }
-  }
-  wapeMoveRight() {
-    this.attackLocation_R.x = this.location.x + 1;
-    this.attackLocation_R.y = this.location.y;
 
-    this.attackLocation_L.x = this.location.x - 1;
-    this.attackLocation_L.y = this.location.y;
-  }
-  wapeMoveLeft() {
-    this.attackLocation_R.x = this.location.x + 1;
-    this.attackLocation_R.y = this.attackLocation_R.y;
-
-    this.attackLocation_L.x = this.location.x - 1;
-    this.attackLocation_L.y = this.attackLocation_L.y;
-  }
-  player_setDirectionY(type) {
-    this.directionY = type;
-  }
-  player_setDirectionX(type) {
-    this.directionX = type;
-  }
-  //整個角色和武器 移動基本設定
-  move(type) {
-    if (type === "ArrowLeft" || type === "ArrowRight") {
-      this.player_setDirectionX(type);
-    }
-    if (type === "ArrowUp" || type === "ArrowDown") {
-      this.player_setDirectionY(type);
-    }
-    if (type === "ArrowUp") {
-      this.player_moveUp();
-    }
-    if (type === "ArrowDown") {
-      this.player_moveDown();
-    }
-    if (type === "ArrowLeft") {
-      this.player_moveLeft();
-      this.wapeMoveLeft();
-    }
-    if (type === "ArrowRight") {
-      this.player_moveRight();
-      this.wapeMoveRight();
-    }
-  }
-
-  //對移動造成的小問題，做些設定
-  player_moveUp2() {
-    if (this.jumpControl != true) {
-      this.location.x = this.location.x;
-      this.location.y = this.location.y + 1;
-      this.jumpControl = true;
-    }
-  }
-  player_moveDown2() {
-    this.scaleControl = false;
-    if (this.scaleControl != true) {
-      this.location.scale = 100;
-    }
-  }
-  move2(type) {
-    if (type === "ArrowDown") {
-      this.player_moveDown2();
-    }
-    if (type === "ArrowUp") {
-      this.player_moveUp2();
-    }
-  }
-  //攻擊設定
-  attackSpace(type) {
-    if (type === "Space") {
-      //蹲下無法攻擊
-      if (type === "Space" && this.location.scale === 50) {
-        return;
-      }
-      //跳躍狀態 無法攻擊
-      if (type === "Space" && this.location.y === 4) {
-        return;
-      }
-
-      //設成true，讓畫面開啟
-      this.attack = true;
-    }
-  }
+}
 }
 
 module.exports = Player;
--- server/game_socket_handler.js
+++ server/game_socket_handler.js
@@ -2,112 +2,134 @@ const game_data_store = require("./models/game_data_store");
 const game_class = require("./game_component/game");
 const player_class = require("./game_component/player");
 let game_data = require("./models/game_data_store");
+
+
 function conn(socket, io) {
+  let time
   socket.on("join-room", (player_name, room_id) => {
-    let game = game_data_store.check_room_exist(room_id);
-    if (!game) {
-      game = new game_class({ id: room_id });
-      game_data_store.init_game_data(room_id, game);
-      var player = new player_class(player_name, room_id);
-      game.add_players(player);
+    if (1) {
+      console.log("check_room_exist");
+      let game = game_data_store.check_room_exist(room_id);
+      if (!game) {
+        game = new game_class({ id: room_id });
+        game_data_store.init_game_data(room_id, game);
+        var player = new player_class({position:{x:0,y:0},velocity:{x:0,y:0}},player_name, room_id);
+        game.add_players(player);
+      }
+      if (!game.check_player_exist(player_name)) {
+        var player = new player_class(
+          player_name,
+          room_id,
+          {
+            x: 9,
+            y: 5,
+            scale: 100,
+          },
+          "player2",
+          "ArrowLeft"
+        );
+        game.add_players(player);
+      }
+      socket.join(room_id);
+      io.sockets.to(room_id).emit("init player", game.players);
     }
-    if (!game.check_player_exist(player_name)) {
-      var player = new player_class(
-        player_name,
-        room_id,
-        {
-          x: 9,
-          y: 5,
-          scale: 100,
-        },
-        "player2",
-        "ArrowLeft"
-      );
-      game.add_players(player);
-    }
-    socket.join(room_id);
-    io.sockets.to(room_id).emit("init player", game.players);
   });
+
+
   socket.on("disconnect", (ev) => {
     console.log("user disconnect:; ", ev);
+    console.log("user disconnect", socket["room"]);
     io.sockets
       .to(socket["room"])
       .emit("room-brocast", `${socket["nick"]} has leave this room`);
   });
+
+
+
   socket.on("player action keydown", (room_id, player_name, type) => {
     let game = game_data.get_game(room_id);
     let player = game.get_player(player_name);
-    player.move(type);
-    player.attackSpace(type);
+    if(type ==='ArrowLeft'){
+        if(player.position.y!=0){
+          return
+      }
+      player.direction='ArrowLeft'
+      player.velocity.x=-5
+      
+    }
+
+    if(type ==='ArrowRight'){
+      if(player.position.y!=0){
+        return
+      }
+      player.direction='ArrowRight'
+      player.velocity.x=5
+    }
+
+    if(type ==='ArrowUp'){
+      player.direction='ArrowUp'
+      player.velocity.y=5
+      
+    }
+    function test(){
+      if((player.direction==='ArrowUp'||player.direction==='')&&player.position.y===0&&player.nojump===false){
+          time = setInterval(()=>{
+              player.move(type)
+              player.revise_move(type)
+              io.sockets.to(room_id).emit("player action keydown", game.players);
+              if(player.position.y===0&&player.nojump===true){
+                  console.log('有我嗎')
+                  clearInterval(time)
+              }
+          },10)
+      }
+    }
+
+    player.move(type)
+    test()
     io.sockets.to(room_id).emit("player action keydown", game.players);
   });
+
+
+
+
   socket.on("player action keyup", (room_id, player_name, type) => {
+    
     let game = game_data.get_game(room_id);
     let player = game.get_player(player_name);
-    if (game.players.length >= 2) {
-      let pl1 = game.players[0];
-      let pl2 = game.players[1];
-      attackTest(pl1, pl2);
+
+    if(type ==='ArrowLeft'){
+      player.velocity.x=0
     }
-    if (type === "Space") player.attack = false;
-    player.move2(type);
+
+    if(type ==='ArrowRight'){
+      player.velocity.x=0
+    }
+
+    if(type ==='ArrowUp'){
+      player.direction=''
+      
+    }
+
+    function test(){
+      if((player.direction==='ArrowUp'||player.direction==='')&&player.position.y===0&&player.nojump===false){
+        console.log('123332',player.direction,player.position.y,player.nojump)
+          time = setInterval(()=>{
+              player.move(type)
+              player.revise_move(type)
+              io.sockets.to(room_id).emit("player action keydown", game.players);
+              if(player.position.y===0&&player.nojump===true){
+                  console.log('有我嗎')
+                  clearInterval(time)
+              }
+          },10)
+      }
+    }
+    
+    test(player,room_id,game.players)
     io.sockets.to(room_id).emit("player action keyup", game.players);
+  
   });
 }
-
-function attackTest(pl1, pl2) {
-  if (
-    pl1.attackLocation_R.x === pl2.location.x &&
-    pl1.directionX === "ArrowRight" &&
-    pl1.attack === true &&
-    pl2.location.y === 5 &&
-    pl2.location.scale === 100
-  ) {
-    pl2.hp = pl2.hp - 10;
-  }
-  if (
-    pl1.attackLocation_L.x === pl2.location.x &&
-    pl1.directionX === "ArrowLeft" &&
-    pl1.attack === true &&
-    pl2.location.y === 5 &&
-    pl2.location.scale === 100
-  ) {
-    pl2.hp = pl2.hp - 10;
-  }
-  if (
-    pl2.attackLocation_R.x === pl1.location.x &&
-    pl2.directionX === "ArrowRight" &&
-    pl2.attack === true &&
-    pl1.location.y === 5 &&
-    pl1.location.scale === 100
-  ) {
-    pl1.hp = pl1.hp - 10;
-  }
-  if (
-    pl2.attackLocation_L.x === pl1.location.x &&
-    pl2.directionX === "ArrowLeft" &&
-    pl2.attack === true &&
-    pl1.location.y === 5 &&
-    pl1.location.scale === 100
-  ) {
-    pl1.hp = pl1.hp - 10;
-  }
-}
-
-//遊玩function
-function chickWin(player1,player2){
-  if(player1.hp===0||player2.hp===0){
-      if(player1.hp===0){
-          player2.isWinner =true;
-          // victory_msg.innerText = 'Player1勝利'
-      }
-      if(player2.hp===0){
-          player1.isWinner =true;
-          // victory_msg.innerText = 'Player2勝利'
-      }
-      return gameStart=false
-  }
-}
-
 
 module.exports.conn = conn;
